92fc8f0f37838e3e75988224f770647ea0116bf3
elsa <elsa-david@laposte.net>
2023-03-14 10:49:27 +0100

    get/set on

--- src/app/student/models/student-model.spec.ts
+++ src/app/student/models/student-model.spec.ts
@@ -4,4 +4,14 @@ describe('StudentModel', () => {
   it('should create an instance', () => {
     expect(new StudentModel()).toBeTruthy();
   });
+
+  it("should have 'Aubert' as last name", () => {
+    const student: StudentModel = new StudentModel()
+    student.lastName = 'Aubert'
+    student.email = 'jl-aub@gmail.com'
+    student.login = "jlaubert"
+    student.password = "toto"
+
+    expect(student.lastName).toBe('Aubert')
+  })
 });
