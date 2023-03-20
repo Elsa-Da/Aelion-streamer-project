20d0d272d10687bea10b7c0ade529f642bbef53d
elsa <elsa-david@laposte.net>
2023-03-20 11:23:17 +0100

    Form and errors implemented

--- src/app/student/add/add.component.ts
+++ src/app/student/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup = new FormGroup({})
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      lastName: [
+        '', // Default value
+        [Validators.required] // Validators function
+      ],
+      firstName: [
+        '',
+        []
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+        ]
+      ],
+      phoneNumber: [
+        '',
+        []
+      ],
+      login: [
+        '', // Default value
+        [Validators.required,
+        Validators.minLength(8)] // Validators function
+      ],
+      password: [
+        '', // Default value
+        [Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)] // Validators function
+      ]
+    })
+  }
+
+  public get c(): { [key: string]: AbstractControl } {
+    return this.form.controls
+  }
+
+  public onSubmit(): void {
+    console.log("Form was submitted")
   }
 
 }
